e68bbac3b27c24d0d63e2431b5d3786070e50aa2
chunhuynh-tapcode <huynhdinhtrung2311@gmail.com>
2025-10-06 21:59:57 +0700

    6/10/2025

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Outfit ,Lusitana } from 'next/font/google';
+
+// export const lusitana = Lusitana({ 
+//     weight: ['400', '700'],
+//     subsets: ['latin'], });
+
+export const outfit = Outfit({ subsets: ['latin'] });
